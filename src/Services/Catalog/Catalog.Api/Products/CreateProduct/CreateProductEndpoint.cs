bcbc65e85a63589578cd30d18fbed5035c122289
Amit Kumar <amitkumar20895381@gmail.com>
2026-06-07 07:29:26 +0530

    setting things up

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,14 @@
+﻿using Carter;
+
+namespace Catalog.Api.Products.CreateProduct
+{
+    public record CreateProductRequest(string Name, List<string> Category, string Description, decimal Price);
+    public record CreateProductResponse(Guid Id);
+    public class CreateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.Api.Models;
+using MediatR;
+
+namespace Catalog.Api.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name,List<string> Category,string Description,decimal Price):ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //business logic
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                Price = command.Price
+
+            };
+            return new CreateProductResult(Guid.NewGuid());
+          //  throw new NotImplementedException();
+        }
+    }
+}
